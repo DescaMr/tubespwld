9a69fcb3530a970018d2ffa346ac878f007ebd33
abdulazizarafah <abdulaziz8arafah@gmail.com>
2024-01-07 19:16:44 +0700

    fix bug delete halaman barang keluar

--- resources/views/BarangKeluar/index.blade.php
+++ resources/views/BarangKeluar/index.blade.php
@@ -34,16 +34,36 @@
                             <td>
 
                                 <x-primary-button tag="a" href="{{route('BarangKeluar.edit', $barangkeluar->id)}}">Edit</x-primary-button>
-                                <form action="{{ route('BarangKeluar.destroy', $barangkeluar->id) }}" method="post">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this barang?')">Delete Barang</button>
-                                </form>
+                                <x-danger-button x-data=""
+                                x-on:click.prevent="$dispatch('open-modal', 'confirm-barangmasuk-deletion')"
+                                x-on:click="$dispatch('set-action', '{{ route('BarangKeluar.destroy', $barangkeluar->id) }}')">
+                                {{ __('Delete') }}
+                            </x-danger-button>
 
                             </td>
                         </tr>
                         @endforeach
                     </x-table>
+                    <x-modal name="confirm-barangmasuk-deletion" focusable maxWidth="xl">
+                        <form method="post" x-bind:action="action" class="p-6">
+                            @csrf
+                            @method('delete')
+                            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                                {{ __('Apakah anda yakin akan menghapus data?') }}
+                            </h2>
+                            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                {{ __('Setelah proses dilaksanakan. Data akan dihilangkan secara permanen.') }}
+                            </p>
+                            <div class="mt-6 flex justify-end">
+                                <x-secondary-button x-on:click="$dispatch('close')">
+                                    {{ __('Cancel') }}
+                                </x-secondary-button>
+                                <x-danger-button class="ml-3">
+                                    {{ __('Delete!!!') }}
+                                </x-danger-button>
+                            </div>
+                        </form>
+                    </x-modal>
                    
                 </div>
             </div>
